c1ec32d48f1dae7f20b839c77055c6bb384ed94a
Kalena Almand <kalmand1057@gmail.com>
2020-12-28 17:00:15 -0600

    added models book and index files

---
+++ create-react-express/models/book.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  authors: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  // url string for thumbnail image
+  image: {
+    type: String,
+    default: ""
+  },
+  link: {
+    type: String,
+    default: "",
+    unique: true
+  }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
